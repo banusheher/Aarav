6c2aa9b91d97d0a67f0f3a7608b79ed4eccd929d
banusheher <72216603+banusheher@users.noreply.github.com>
2022-04-01 16:28:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var ball
+function setup() {
+  createCanvas(400, 400);
+  //ball = createSprite(200,200,20,20);
+}
+
+function draw() {
+  background(0);
+  fill("red")
+  textSize(40);
+  text("Hello Aarav", 50,200)
+//drawSprite();
+}
